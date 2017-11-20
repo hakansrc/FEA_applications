aba571f3d58c1ed067edb138131c4359f6455f01
MSI\hakan <hakansrc@msn.com>
2017-11-20 11:23:27 +0300

    syncing

--- week2/transformerdesign.m
+++ week2/transformerdesign.m
@@ -1,7 +1,8 @@
 clear
+clc
 t = 0:0.001:10;
 
-Bmax = 1.5; %tesla
+Bmax = [0.6 0.8 1 1.2 1.5]; %tesla
 f = 50;
 Vpri = 34500;
 Vsec = 400;
@@ -21,12 +22,12 @@ u = u0*ur;
 % all values are known except for area
 % then
 Emax = 400*sqrt(2);
-Area = Emax/(2*pi*f*N2*Bmax);
-flux = Bmax*Area;
+Area = Emax./(2*pi*f*N2.*Bmax);
+flux = Bmax.*Area;
 
 Emax2 = 34500*sqrt(2);
-Area2 = Emax2/(2*pi*f*N1*Bmax);
-flux2 = Bmax*Area2;
+Area2 = Emax2./(2*pi*f*N1.*Bmax);
+flux2 = Bmax.*Area2;
 
 %assuming square core, one side width and height of the core will be
 depth = sqrt(Area);
@@ -47,7 +48,7 @@ total_copper_area = (copper_area_pri_total + copper_area_sec_total)*10^-6; %m2
 inner_space_area = total_copper_area/0.6; %m2
 inner_space_side_length = sqrt(inner_space_area);
 outer_side_length = inner_space_side_length + 2*width;
-outer_space_area = outer_side_length^2;
+outer_space_area = outer_side_length.^2;
 
 
 side_length_effective = outer_side_length-width;
@@ -66,18 +67,20 @@ reluctance = length_effective/(u*Area);
 
 %core&coppermass
 core_surface_area = outer_space_area - inner_space_area ; %m2
-core_volume = core_surface_area * depth; %m3
+core_volume = core_surface_area .* depth; %m3
 core_density = 7750 ;%kg/m3
 core_mass = core_volume*core_density/1000; %tons
 core_material_cost = 3 ;% $/kg
-core_total_cost = core_mass*1000*core_material_cost; 
-core_losses = 2.5*core_mass*1000; % watts
+core_total_cost = core_mass*1000*core_material_cost;
+core_loss_values = [0.5 0.8 1 1.5 2.5];
+
+    
+core_losses = core_loss_values.*core_mass*1000; % watts
 
 
-
-copper_volume = N1*(width*4)*(copper_area_pri_percoil*10^-6) + N2*(width*4)*(copper_area_sec_percoil*10^-6); %m3
-copper_length_pri = 4*width*N1;
-copper_length_sec = 4*width*N2;
+copper_volume = N1.*(width.*4).*(copper_area_pri_percoil.*10^-6) + N2.*(width.*4).*(copper_area_sec_percoil.*10^-6); %m3
+copper_length_pri = 4*width.*N1;
+copper_length_sec = 4*width.*N2;
 copper_density = 8960 ;% kg/m3
 copper_mass = copper_volume*copper_density; %kg
 copper_material_cost = 10 ; %$/kg
@@ -87,7 +90,7 @@ copper_resistivity = 1.7*10^-8;
 copper_resistivity_pri = copper_length_pri*copper_resistivity/(copper_area_pri_percoil*10^-6);
 copper_resistivity_sec = copper_length_sec*copper_resistivity/(copper_area_sec_percoil*10^-6);
 
-Lm = N1^2/reluctance;
+Lm = N1.^2./reluctance;
 
 copper_losses = Ipri^2*copper_resistivity_pri+Isec^2*copper_resistivity_sec;
 total_losses = core_losses+copper_losses;
@@ -95,13 +98,15 @@ total_losses = core_losses+copper_losses;
 Pout = 1000000; %VA
 Pin = Pout+total_losses;
 
-efficiency = Pout/Pin*100; %percent
+efficiency = Pout./Pin*100; %percent
 
+total_cost = copper_total_cost + core_total_cost;
+total_mass = copper_mass + core_mass*1000;
+%for a = 1:5
+Lm = [Lm Lm Lm Lm Lm];
+N1 = [N1 N1 N1 N1 N1];
+N2 = [N2 N2 N2 N2 N2];
+results = table(transpose(Bmax),core_mass',copper_mass',copper_resistivity_pri',copper_resistivity_sec',Lm',core_losses',copper_losses',efficiency',total_cost');
 
-
-
-
-
-
-
-
+filename = 'results.xlsx';
+writetable(results,filename,'Sheet',1)
